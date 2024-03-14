4fa44b9026db960355b0a5955b25696472fcaa08
abdellahak <93621937+abdellahak@users.noreply.github.com>
2024-03-14 22:49:31 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function afficheCookies(){
+    document.getElementById('cookies').style.display ='block';
+
+}
+
+function fermerCookies(){
+    document.getElementById('cookies').style.display ='none';
+    
+}
