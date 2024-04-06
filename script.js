d9326065cbcb841b7747cf742ac0abefee515749
abdellahak <93621937+abdellahak@users.noreply.github.com>
2024-04-06 09:40:45 +0000

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function afficheCookies(){
-    document.getElementById('cookies').style.display ='block';
-
-}
-
-function fermerCookies(){
-    document.getElementById('cookies').style.display ='none';
-    
-}
